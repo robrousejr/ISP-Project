0c89cd423eb524bfa185038323193b4f086d7a8a
Main Desktop <robrousejr@gmail.com>
2019-11-23 17:35:03 -0500

    Add 'Alata' font

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/helpmodal.css">
         <link rel="stylesheet" type="text/css" href="css/musiccontainer.css">
+        <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet">
     </head>
 <body>
 
