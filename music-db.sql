8189da11a4922ba22fdc7e112743b19123429f75
Main Desktop <robrousejr@gmail.com>
2019-11-24 16:21:40 -0500

    Update music database

--- music-db.sql
+++ music-db.sql
@@ -2,13 +2,14 @@ DROP TABLE IF EXISTS Music;
 
 CREATE TABLE Music (
     Song_id INT(11) NOT NULL AUTO_INCREMENT,
-    Song_name CHAR(15),
-    Artist CHAR(15),
-    Album CHAR(15),
+    Song_name CHAR(30),
+    Artist CHAR(30),
+    Album CHAR(30),
     Year INT(4),
     PRIMARY KEY (Song_id)
 );
 
 insert into Music values
-(1, 'SongName', 'Band', 'AlbumName', 2000),
-(2, 'Song2', 'Band2', 'Album2', 2004);
+(1, 'Billie Jean', 'Michael Jackson', 'Thriller', 1982),
+(2, 'Hey Jude', 'The Beatles', 'Hey Jude', 1968),
+(3, 'Tiny Dancer', 'Elton John', 'Madman Across the Water', 1971);
