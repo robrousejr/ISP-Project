994172c0c8bab0df2f46ce102cd4ee3d86a1d02a
Main Desktop <robrousejr@gmail.com>
2019-11-27 17:35:09 -0500

    Name song correctly in directory

--- addSong.php
+++ addSong.php
@@ -12,8 +12,16 @@
     if($result)
     {
         $target_dir = "music/";
-        $musicFile = $target_dir . basename($_FILES['songUpload']['name']);
-        move_uploaded_file($_FILES["songUpload"]["tmp_name"], $musicFile);
+        $musicFile = $target_dir . basename($_FILES['songUpload']['name']); // Access uploaded .mp3 file
+
+        // Get amount of songs in table
+        $rowCountResult = mysqli_query($conn, "SELECT * FROM music");
+        $numSongs = mysqli_num_rows($rowCountResult);
+
+        // Put song in 'music' directory with correct name
+        $newSongFilename = (strval($numSongs) . ".mp3");
+        move_uploaded_file($_FILES["songUpload"]["tmp_name"], $target_dir . $newSongFilename);
+
 
         header("Location: http://localhost/isp-project"); // Redirect back
         exit(); // Stops any code below from executing
